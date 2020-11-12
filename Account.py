7a0012f55880d0ac351b451637fd5fcfba62245c
何金泽 | Jinze He <hejinze_finance@pku.edu.cn>
2020-11-12 11:57:55 +0800

    almost finish the backtest platform, except for plot_nav()

--- Account.py
+++ Account.py
@@ -1,20 +1,97 @@
 import pandas as pd
+import numpy as np
+from datetime import datetime as dt, timedelta as td
+
+
 class Account:
-    def __init__(self, initial_cash, stamp_tax_rate,codes_all):
-        self.cash_amount = initial_cash
+    def __init__(self, codes_all, start_time):
+        # 超参数默认值
+        self.initial_cash = 1e8
+        self.tax_rate = 0.001
+        # 临时变量
+        self.curr_time = start_time
+        self.last_time = start_time - td(1)
+        self.last_price = None
+        self.new_price = None
+        # 重要变量
+        self.codes_all = codes_all
+        self.start_time = start_time
+        self.cash_amount = pd.Series(self.initial_cash, index=[self.last_time])
+        self.before_trading_amount = pd.DataFrame(data=np.zeros((1, self.codes_all.shape[0])),
+                                                  index=[self.start_time], columns=self.codes_all)
+        self.after_trading_amount = pd.DataFrame(index=[self.start_time], columns=self.codes_all)
+        self.holding_quant = pd.DataFrame(index=[self.last_time, self.start_time], columns=self.codes_all)
+        self.holding_quant.loc[self.last_time] = np.zeros((1, self.codes_all.shape[0]))
+        self.pnl_log = pd.DataFrame(index=[self.start_time], columns=self.codes_all)
+        self.pnl_series = pd.Series(index=[self.start_time])
+        self.stamp_cost = pd.Series(index=[self.start_time])
+        self.nav_before_trading = pd.Series(self.initial_cash, index=[self.start_time])
+        self.nav_after_trading = pd.Series(index=[self.start_time])
+
+    # 超参数设定
+    def settings(self, initial_cash, stamp_tax_rate):
+        self.initial_cash = initial_cash
         self.tax_rate = stamp_tax_rate
-        self.curr_data = pd.DataFrame(index = codes_all, columns = ['price', 'quantity', 'amount'])
-        self.exp_quantity = pd.Series(index = codes_all)
-        self.next_price = pd.Series(index = codes_all)
 
-    def getExpQuantity(self, exp_quantity):
-        self.exp_quantity = exp_quantity
+    # 重置函数
+    def reset(self):
+        self.curr_time = self.start_time
+        self.last_time = self.start_time - dt(1)
+        self.last_price = None
+        self.new_price = None
+        self.before_trading_amount = pd.DataFrame(data=np.zeros((1, self.codes_all.shape[0])),
+                                                  index=[self.start_time], columns=self.codes_all)
+        self.after_trading_amount = pd.DataFrame(index=[self.start_time], columns=self.codes_all)
+        self.holding_quant = pd.DataFrame(index=[self.last_time, self.start_time], columns=self.codes_all)
+        self.holding_quant.loc[self.last_time] = np.zeros((1, self.codes_all.shape[0]))
+        self.pnl_log = pd.DataFrame(index=[self.start_time], columns=self.codes_all)
+        self.pnl_series = pd.Series(index=[self.start_time])
+        self.stamp_cost = pd.Series(index=[self.start_time])
+        self.cash_amount = pd.Series(self.initial_cash, index=[self.last_time])
 
-    def getNextPrice(self,next_price):
-        self.next_price = next_price
-
-    def executeOrder(self):
-        pass
+    def executeOrder(self, curr_orders, prices):
+        self.new_price = prices
+        self.holding_quant.loc[self.curr_time] = self.holding_quant.loc[self.last_time] + curr_orders
+        self.stamp_cost.loc[self.curr_time] = -prices.T @ curr_orders.map(self.negamere) * self.tax_rate
+        self.cash_amount.loc[self.curr_time] = self.cash_amount.loc[self.last_time] - prices.T @ curr_orders - \
+                                               self.stamp_cost.loc[self.curr_time]
+        self.after_trading_amount.loc[self.curr_time] = prices.T * self.holding_quant.loc[self.curr_time]
+        self.nav_after_trading.loc[self.curr_time] = self.after_trading_amount.loc[self.curr_time].sum() + \
+                                                     self.cash_amount.loc[self.curr_time]
 
     def calStampCost(self):
         pass
+
+    def getCurrPos(self, curr_time):
+        output = dict()
+        curr_quant = self.holding_quant.loc[self.last_time].T
+        curr_amount = self.before_trading_amount.loc[curr_time].T
+        output['trading_securities'] = pd.concat([curr_quant, curr_amount], axis=1)
+        output['trading_securities'].columns = ['quant', 'amount']
+        output['cash'] = self.cash_amount.loc[self.last_time]
+        output['nav'] = self.nav_before_trading.loc[curr_time]
+        return output
+
+    def calLastTime(self, curr_time):
+        curr_idx = self.holding_quant.index.get_loc(curr_time)
+        return self.holding_quant.index[curr_idx - 1]
+
+    @staticmethod
+    def orthomere(element):
+        return max(element, 0)
+
+    @staticmethod
+    def negamere(element):
+        return min(element, 0)
+
+    def recordPnL(self, new_time, new_prices):
+        self.last_time = self.curr_time
+        self.curr_time = new_time
+        self.last_price = self.new_price
+        self.new_price = new_prices
+        self.before_trading_amount.loc[self.curr_time] = new_prices * self.holding_quant.loc[self.last_time]
+        self.nav_before_trading.loc[self.curr_time] = self.before_trading_amount.loc[self.curr_time].sum() + \
+                                                      self.cash_amount.loc[self.last_time]
+        self.pnl_log.loc[self.curr_time] = self.before_trading_amount.loc[self.curr_time] - \
+                                           self.after_trading_amount.loc[self.last_time]
+        self.pnl_series.loc[self.curr_time] = self.pnl_log.loc[self.curr_time].sum()
--- Backtest.py
+++ Backtest.py
@@ -9,16 +9,102 @@ class Backtest:
     def __init__(self, price_mat_data, code_name):
         # 重要成员
         self.price_mat_data = price_mat_data
-        self.code_name = pd.Series(index = price_mat_data.columns, value = code_name)
+        self.code_name = pd.Series(data=code_name.values, index=price_mat_data.columns)
         self.initConstParas()
-        self.reset()
-        # 超参数
-        self.initial_cash = 1e8
-        self.stamp_tax_rate = 0.001
+        self.initAccount()
 
-    def settings(self,initial_cash,stamp_tax_rate):
-        self.initial_cash = initial_cash
-        self.stamp_tax_rate = stamp_tax_rate
+    def setAccount(self, initial_cash, stamp_tax_rate):
+        self.account.settings(initial_cash, stamp_tax_rate)
+
+    def runBackTest(self):
+        for idx, curr_time in enumerate(self.times_all):
+            self.curr_time = curr_time
+            if self.curr_time == self.start_time:
+                pass
+            else:
+                self.account.recordPnL(self.curr_time, self.price_mat_data.loc[self.curr_time])
+            if self.curr_time == self.end_time:
+                self.finishStrategy()
+            curr_pos_info = self.account.getCurrPos(self.curr_time)
+            target_weight = self.normalize(self.target_weights.loc[curr_time])
+            if curr_pos_info['nav'] <= 0:
+                self.bankCruptsy()
+            curr_orders = self.generateOrders(curr_pos_info, target_weight)
+            self.account.executeOrder(curr_orders, self.price_mat_data.loc[self.curr_time])
+            if idx % 30 == 0:
+                print(self.curr_time)
+
+    def generateOrders(self, curr_pos_info, target_weight):
+        curr_weight = curr_pos_info['trading_securities'].amount / curr_pos_info['nav']
+        diff_weight = target_weight - curr_weight
+        signs = diff_weight.map(self.sign)
+        deducted_value = (diff_weight.loc[signs == -1] * curr_pos_info['nav'])
+        deducted_quant = self.quantFloor(
+            deducted_value / self.price_mat_data.loc[self.curr_time, signs == -1])  # 多卖点，克服印花税的削弱作用
+        left_money = curr_pos_info['cash'] - (
+            self.price_mat_data.loc[self.curr_time, signs == -1].T) @ deducted_quant * (1 - self.account.tax_rate)
+        increased_value = self.normalize(diff_weight.loc[signs == 1]) * left_money
+        increased_quant = self.quantFloor(
+            increased_value / self.price_mat_data.loc[self.curr_time, signs == 1])  # 少买点，怕钱不够
+        trading_quant = pd.concat([deducted_quant, increased_quant], axis=0).sort_index()
+        return trading_quant
+
+    def bankCruptsy(self):
+        print('破产了！！！')
+
+    def finishStrategy(self):
+        self.max_drawback_ratios = self.calMD(self.account.nav_after_trading)
+        self.calMetrics()
+        self.identifyTrend()
+
+    def identifyTrend(self, width=5):
+        moving_average_center = self.account.nav_after_trading.rolling(5, center=True, min_periods=1).mean()
+        self.trends = moving_average_center.diff().fillna(1).map(self.sign)
+
+    def calMetrics(self):
+        self.strategy_metrics = dict()
+        self.strategy_metrics['AR'] = np.power(
+            self.account.nav_after_trading.iloc[-1] / self.account.nav_after_trading.iloc[0],
+            self.account.nav_after_trading.shape[0] / 252)
+        self.strategy_metrics['AV'] = self.account.nav_after_trading.pct_change().dropna().std() * np.sqrt(252)
+        self.strategy_metrics['SR'] = self.strategy_metrics['AR'] / self.strategy_metrics['AV']
+        self.strategy_metrics['MD'] = self.max_drawback_ratios.max()
+        self.strategy_metrics['CMR'] = self.strategy_metrics['AR'] / self.strategy_metrics['MD']
+        win_lose = self.account.nav_after_trading.diff().dropna().map(self.sign)
+        self.strategy_metrics['WR'] = (win_lose == 1).sum() / win_lose.shape[0]
+
+    def plot_nav(self, png_path='/figures/', file_name='performance.png'):
+        plt.figure(figsize=(12, 6))
+        plt.plot(self.account.nav_after_trading)
+
+    def getWeights(self, weights):
+        self.target_weights = weights
+
+    @classmethod
+    def calMD(cls, nav_series):
+        nav_max_series = cls.calHistoryMax(nav_series)
+        drawdown = nav_max_series - nav_series
+        max_drawdown = cls.calHistoryMax(drawdown)
+        max_drawdown_ratio = max_drawdown / nav_max_series
+        return max_drawdown_ratio
+
+    @staticmethod
+    def calHistoryMax(series_data):
+        max_value = -float('inf')
+        hist_max_data = series_data.copy()
+        for curr_time in series_data.index:
+            new_value = series_data.loc[curr_time]
+            if new_value > max_value:
+                max_value = new_value
+            hist_max_data.loc[curr_time] = max_value
+        return hist_max_data
+
+    @staticmethod
+    def sign(element):
+        if element >= 0:
+            return 1
+        else:
+            return -1
 
     def initConstParas(self):
         self.codes_all = self.price_mat_data.columns
@@ -28,11 +114,21 @@ class Backtest:
 
     def reset(self):
         self.curr_time = self.start_time
-        self.curr_exp_return = pd.Series(index = self.codes_all)
-        self.pnl_intervals = pd.Series(index = self.times_all[1:])
-        self.stamp_cost = pd.Series(index = self.times_all[1:])
-        self.exp_returns = pd.DataFrame(index = self.times_all[1:], columns = self.codes_all)
-        self.initAccount()
+        self.max_drawback_ratio = None
+        self.strategy_metrics = dict()
+        self.account.reset()
 
     def initAccount(self):
-        self.account = Account(self.initial_cash, self.stamp_tax_rate, self.codes_all)
+        self.account = Account(self.codes_all, self.start_time)
+
+    @staticmethod
+    def normalize(vector):
+        return vector / sum(vector)
+
+    @staticmethod
+    def quantFloor(quant):
+        return np.floor(quant / 100) * 100
+
+    @staticmethod
+    def quantCeil(quant):
+        return np.ceil(quant / 100) * 100
